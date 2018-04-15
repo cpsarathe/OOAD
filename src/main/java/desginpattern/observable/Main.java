a6a67e80a2ac1dc553a903123f910d4838a01f78
Chandraprakash Sarathe <Chandra.Sarathe@emaratech.ae>
2018-04-15 21:17:26 +0400

    base structure

---
+++ src/main/java/desginpattern/observable/Main.java
@@ -0,0 +1,11 @@
+package desginpattern.observable;
+
+/**
+ * Created by Chandra.Sarathe on 4/15/2018.
+ */
+public class Main {
+
+    public static void main(String ar[]){
+        System.out.println("Hello");
+    }
+}
